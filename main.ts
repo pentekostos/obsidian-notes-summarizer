d21cab49abcf38ef9eb7d5463d4742feca2fe58b
Matthew Barlow <mb@mattjbarlow.com>
2023-05-12 21:06:02 -0400

    make days configuragble

--- main.ts
+++ main.ts
@@ -3,10 +3,12 @@ import { TFile } from 'obsidian';
 
 interface SummarizerSettings {
 	GPTAPIKey: string;
+	numRecentDays: number;
 }
 
 const DEFAULT_SETTINGS: SummarizerSettings = {
-	GPTAPIKey: ''
+	GPTAPIKey: '',
+	numRecentDays: 7
 }
 
 export default class SummarizerPlugin extends Plugin {
@@ -16,43 +18,35 @@ export default class SummarizerPlugin extends Plugin {
 	async onload() {
 		await this.loadSettings();
 		this.addCommand({
-		  id: 'get-modified-files-content',
-		  name: 'Get Modified Files Content',
-		  checkCallback: (checking: boolean) => {
-			if (!checking) {
-			  this.getModifiedFilesContent().then((content) => {
-				generateSummary(this.settings.GPTAPIKey, content).then((summary) => {
-				  console.log("sending request to GPT");
-				  console.log(summary);
-	  
-				  // Get the active view and editor
-				  const activeView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				  if (activeView) {
-					const editor = activeView.editor;
-	  
-					// Replace the selected text with the summary
-					editor.replaceSelection(summary);
-	  
-					// Display a message
-					new Notice('Summary added to the selection buffer');
-				  } else {
-					new Notice('No active Markdown view found');
-				  }
-				});
-			  });
-			}
-			return true;
-		  },
+			id: 'get-modified-files-content',
+			name: 'Get Modified Files Content',
+			checkCallback: (checking: boolean) => {
+				if (!checking) {
+					this.getModifiedFilesContent().then((content) => {
+						generateSummary(this.settings.GPTAPIKey, content).then((summary) => {
+							console.log("sending request to GPT");
+							console.log(summary);
+
+							// Copy the summary to the clipboard
+							navigator.clipboard.writeText(summary).then(() => {
+								// Notification when the response from GPT-4 is ready
+								new Notice('Summary copied to clipboard');
+							}).catch(err => {
+								console.error('Could not copy text: ', err);
+							});
+						});
+					});
+				}
+				return true;
+			},
 		});
 		this.addSettingTab(new SummarizerSettingTab(this.app, this));
-	  }
-	  
-	  
-
+	}
 
 	async getModifiedFilesContent(): Promise<string> {
 		const currentTime = new Date().getTime();
-		const oneWeekAgo = currentTime - 14 * 24 * 60 * 60 * 1000;
+		const numRecentDays = this.settings.numRecentDays;
+		const recentTime = currentTime - numRecentDays * 24 * 60 * 60 * 1000;
 		const files = this.app.vault.getFiles();
 
 		let content = '';
@@ -61,7 +55,7 @@ export default class SummarizerPlugin extends Plugin {
 			if (file instanceof TFile) {
 				const fileCtime = new Date(file.stat.ctime);
 
-				if (fileCtime.getTime() > oneWeekAgo) {
+				if (fileCtime.getTime() > recentTime) {
 					const fileContent = await this.app.vault.read(file);
 					content += fileContent + '\n';
 				}
@@ -70,6 +64,8 @@ export default class SummarizerPlugin extends Plugin {
 
 		return content;
 	}
+
+
 
 	async loadSettings() {
 		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
@@ -102,14 +98,14 @@ async function generateSummary(apiKey: string, content: string): Promise<string>
 		};
 
 		const payload = {
-			model: 'gpt-4-0314',
+			model: 'gpt-4',
 			messages: [
 				{
 					role: 'user',
 					content: `Summarize this content. Use Markdown format. Add flascards and footnotes at the bottom.: ${content}`,
-					max_tokens: 16000,
 				},
 			],
+			max_tokens: 4000,
 		};
 
 		xhr.send(JSON.stringify(payload));
@@ -140,6 +136,17 @@ class SummarizerSettingTab extends PluginSettingTab {
 				.setValue(this.plugin.settings.GPTAPIKey)
 				.onChange(async (value) => {
 					this.plugin.settings.GPTAPIKey = value;
+					await this.plugin.saveSettings();
+				}));
+
+		new Setting(containerEl)
+			.setName('Number of Recent Days')
+			.setDesc('Number of recent days to include in the summary')
+			.addText(text => text
+				.setPlaceholder('Number of Recent Days')
+				.setValue(String(this.plugin.settings.numRecentDays)) // Convert the number to a string for the text field
+				.onChange(async (value) => {
+					this.plugin.settings.numRecentDays = Number(value); // Convert the input string back to a number
 					await this.plugin.saveSettings();
 				}));
 	}
